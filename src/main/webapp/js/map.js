bb89326ba526c600a48722a0425bd569bf0a287a
Dave Cohen <dcohen@infinio.com>
2016-01-20 16:57:13 -0500

    Map stuff!

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -17,12 +17,48 @@
                     color: "#fff"
                 }).html(chr);
                 return elem;
-            }
+            },
+            onEdit: function(ev, elem) {
+                var $elem = $(elem);
+                $('#NoteDialog').remove();
+                return $('<div id="NoteDialog"></div>').dialog({
+                    title: "Note Editor",
+                    resizable: false,
+                    modal: true,
+                    height: "300",
+                    width: "450",
+                    position: { my: "left bottom", at: "right top", of: elem},
+                    buttons: {
+                        "Save": function() {
+                            var txt = $('textarea', this).val();
+//			Put the editied note back into the data area of the element
+//			Very important that this step is included in custom callback implementations
+                            $elem.data("note", txt);
+                            $(this).dialog("close");
+                        },
+                        "Delete": function() {
+                            console.log("Deleted!")
+                            $elem.trigger("remove");
+                            $(this).dialog("close");
+                        },
+                        Cancel: function() {
+                            $(this).dialog("close");
+                        }
+                    },
+                    open: function() {
+                        $(this).css("overflow", "hidden");
+                        var textarea = $('<textarea id="txt" style="height:100%; width:100%;">');
+                        $(this).html(textarea);
+//			Get the note text and put it into the textarea for editing
+                        textarea.val($elem.data("note"));
+                    }
+                });
+            },
         });
         $img.imgNotes("import", [
-            {x: "0.5", y: "0.5", note: "AFL Grand Final Trophy"},
-            {x: "0.322", y: "0.269", note: "Brisbane Lions Flag"},
-            {x: "0.824", y: "0.593", note: "Fluffy microphone"}
+            {x: "0.5", y: "0.5", note: "The Greenwall"},
+            {x: "0.322", y: "0.269", note: "Some Dwarves, I Guess"},
+            {x: "0.824", y: "0.593", note: "Magical River"}
         ]);
     });
 })(jQuery);
